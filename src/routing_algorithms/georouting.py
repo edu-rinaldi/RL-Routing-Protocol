bad1ecb2f747e4561c6f9f84643d876cad4904a0
Andrea94c <andreacoletta94@live.it>
2020-11-19 11:36:57 +0100

    Add Geographic Routing with expected distance

--- src/routing_algorithms/georouting.py
+++ src/routing_algorithms/georouting.py
@@ -1,5 +1,7 @@
 
+import numpy as np
 import src.utilities.utilities as util
+
 from src.routing_algorithms.BASE_routing import BASE_routing
 
 class GeoRouting(BASE_routing):
@@ -8,4 +10,40 @@ class GeoRouting(BASE_routing):
 
     def relay_selection(self, opt_neighbors):
         """ arg min score  -> geographical approach, take the drone closest to the depot """
-        pass
+        best_drone_distance_from_depot = util.euclidean_distance(self.simulator.depot.coords, self.drone.coords)
+        best_drone = None
+
+        for hpk, drone_istance in opt_neighbors:
+            exp_position = self.__estimated_neighbor_drone_position(hpk)
+            exp_distance = util.euclidean_distance(exp_position, self.simulator.depot.coords)
+            if exp_distance < best_drone_distance_from_depot:
+                best_drone_distance_from_depot = exp_distance
+                best_drone = drone_istance
+
+        return best_drone
+
+
+    def __estimated_neighbor_drone_position(self, hello_message):
+        """ estimate the current position of the drone """
+
+        # get known info about the neighbor drone
+        hello_message_time = hello_message.time_step_creation
+        known_position = hello_message.cur_pos
+        known_speed = hello_message.speed
+        known_next_target = hello_message.next_target
+
+        # compute the time elapsed since the message sent and now
+        # elapsed_time in seconds = elapsed_time in steps * step_duration_in_seconds
+        elapsed_time = (self.simulator.cur_step - hello_message_time) * self.simulator.time_step_duration  # seconds
+
+        # distance traveled by drone
+        distance_traveled = elapsed_time * known_speed
+
+        # direction vector
+        a, b = np.asarray(known_position), np.asarray(known_next_target)
+        v_ = (b - a) / np.linalg.norm(b - a)
+
+        # compute the expect position
+        c = a + (distance_traveled * v_)
+
+        return tuple(c)
