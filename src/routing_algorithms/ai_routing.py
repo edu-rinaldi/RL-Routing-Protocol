202519b973633d0321d7a83a96e774a3dfd5c888
Eduardo Rinaldi <eduardo@Air-di-Eduardo.homenet.telecomitalia.it>
2020-12-19 15:17:11 +0100

    added method selection

--- src/routing_algorithms/ai_routing.py
+++ src/routing_algorithms/ai_routing.py
@@ -6,17 +6,25 @@ from src.routing_algorithms.BASE_routing import BASE_routing
 from matplotlib import pyplot as plt
 
 class AIRouting(BASE_routing):
-    def __init__(self, drone, simulator):
+    def __init__(self, drone, simulator, method='drone_path'):
         BASE_routing.__init__(self, drone, simulator)
         # random generator
         self.rnd_for_routing_ai = np.random.RandomState(self.simulator.seed)
         self.taken_actions = {}  #id event : (old_state, old_action)
 
+        # method for state representation
+        # method = 'pos_perc'
+        self.method = method
+
         # Q-Table
-        self.q_table = self.rnd_for_routing_ai.uniform(low=-2, high=0, size=[len(self.drone.path), self.simulator.n_drones])
-        self.epsilon = 0.3
+        if method == 'drone_path':
+            self.q_table = self.rnd_for_routing_ai.uniform(low=-2, high=0, size=[len(self.drone.path), self.simulator.n_drones])
+        elif method == 'pos_perc':
+            self.pos_precision = 25
+            self.q_table = self.rnd_for_routing_ai.uniform(low=-2, high=0, size=[self.pos_precision, self.simulator.n_drones])
+        self.epsilon = 0.08
         self.alpha = 0.2
-        self.gamma = 0.45
+        self.gamma = 0.4
 
         self.r_sum = 0
         self.timestep = 0
@@ -57,7 +65,6 @@ class AIRouting(BASE_routing):
             
             
             # update using the old state and the selected action at that time
-            # self.q_table[(state, ) + (action, )] += self.alpha * (reward + self.gamma * self.q_table[(new_state, ) + (new_action, )])
             # do something or train the model (?)
 
     def get_reward(self, outcome, delay):
@@ -66,7 +73,12 @@ class AIRouting(BASE_routing):
         return 2 * (1 - delay/self.simulator.event_duration)
 
     def get_state(self):
-        return self.drone.path.index(self.drone.next_target())
+        if self.method == 'drone_path':
+            return self.drone.path.index(self.drone.next_target())
+        if self.method == 'pos_perc':
+            perc = self.__get_curr_pos_percentage() * 100
+            perc //= (100//self.pos_precision)
+            return int(perc)
         
     def get_action(self, state, available_drones):
         max_drone = None
@@ -90,15 +102,15 @@ class AIRouting(BASE_routing):
         #                                                y_pos=self.drone.coords[1])[0]  # e.g. 500
         # print(cell_index)
         state = self.get_state()
-        available_drones = [self.drone] + list(map(lambda x: x[1], opt_neighbors))
+        available_drones =  [self.drone] + list(map(lambda x: x[1], opt_neighbors))
         action, best_drone = self.get_action(state, available_drones)
-        # print(available_drones)
+
         # Store your current action --- you can add several stuff if needed to take a reward later
         self.taken_actions[pkd.event_ref.identifier] = (state, action)
-
+        
         if self.rnd_for_routing_ai.rand() < self.epsilon:
             return self.rnd_for_routing_ai.choice(available_drones)
-
+        
         return best_drone
 
     def __get_path_size(self):
@@ -116,14 +128,13 @@ class AIRouting(BASE_routing):
             p1 = self.drone.path[i]
             p2 = self.drone.path[i+1]
             if f: break
-            if p2 == point: f= True
+            if p2 == point: f = True
 
             total += util.euclidean_distance(p1, p2)
         return total
 
     def __get_curr_pos_percentage(self):
         total = self.__get_path_size()
-        # print(self.drone.next_target(), self.__get_path_size_point(self.drone.next_target()), self.drone.path)
         d = self.__get_path_size_point(self.drone.next_target()) - np.abs(util.euclidean_distance(self.drone.coords, self.drone.next_target()))
         return d/total
 
